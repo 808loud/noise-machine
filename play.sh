96c1ba8c57a43eaec2a732255a4fd47c27fe1e9d
David Monson <david@808loud.com>
2017-02-11 01:54:51 +0000

    easier copy/pasting, use bluetooth audio out

--- play.sh
+++ play.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-len='12:00:00'
+# Going to use bluetooth dongle instead, so commenting out these lines for now
+#export AUDIODRIVER=alsa
+#export AUDIODEV=hw:0,0
 
-export AUDIODRIVER=alsa
-export AUDIODEV=hw:0,0
-
-play -c 2 -b 16 -r 41000 -t sl - synth $len brownnoise band -n 4000 2500 tremolo 60 5 < /dev/zero
+# play -c $numberOfChannels -b $bitRate -r sampleRate -t sl - synth $duration brownnoise tremolo $tremoloSpeed $tremoloDepth gain $gainDecibels < $outputFile
+play -c 2 -b 16 -r 44.1k -t sl - synth 12:00:00 brownnoise tremolo 11 0.5 gain -9 < /dev/zero
