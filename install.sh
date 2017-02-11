e1f4f05d8f20870dd92e1144343c4a64d4335d49
David Monson <david@808loud.com>
2017-02-11 02:00:42 +0000

    documentation for cron job

--- install.sh
+++ install.sh
@@ -7,4 +7,5 @@
 DIR="$( cd "$(dirname $0)" && pwd )"
 
 # set cron job to run play script from same directory
+# job will run daily at 9:00 PM (21 in 24-hour format)
 ( crontab -l ; echo "0 21 * * * ${DIR}/play.sh" ) | crontab -
