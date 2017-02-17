4047fd2283fa62d7d1f982f5964f9a6489fe5b90
David Monson <david@808loud.com>
2017-02-16 18:00:42 -0600

    simplify the null file input argument

--- play.sh
+++ play.sh
@@ -5,4 +5,4 @@
 #export AUDIODEV=hw:0,0
 
 # play -c $numberOfChannels -b $bitRate -r sampleRate -t sl - synth $duration brownnoise tremolo $tremoloSpeed $tremoloDepth gain $gainDecibels < $outputFile
-/usr/bin/play -c 2 -b 16 -r 44.1k -t sl - synth 12:00:00 brownnoise tremolo 11 0.5 gain -9 < /dev/zero
+/usr/bin/play -c 2 -b 16 -r 44.1k -n synth 12:00:00 brownnoise tremolo 11 0.5 gain -9 < /dev/zero
