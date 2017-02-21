7a023e0b01346dae687c97dd3c7873fcbdd90728
David Monson <david@808loud.com>
2017-02-21 17:43:18 -0600

    update fade syntax to historical spec, update docs

--- play.sh
+++ play.sh
@@ -7,8 +7,11 @@
 #export AUDIODRIVER=alsa
 #export AUDIODEV=hw:0,0
 
+# Documentation
+# http://sox.sourceforge.net/sox.html
 # --null is the input file needed to generate random noise (using synth)
+# fade $typeOfFade $fadeInLength $stopPosition
 # tremolo $tremoloSpeed $tremoloDepth
 # gain $gainDecibels
 # output is piped to /dev/zero as we don't need sox to generate a file
-/usr/bin/play --channels 2 --rate 44.1k --null synth 11:00:00 brownnoise fade q 30 -0 tremolo 11 0.5 gain -15 < /dev/zero
+/usr/bin/play --channels 2 --rate 44.1k --null synth 11:00:00 brownnoise fade q 30 0 tremolo 11 0.5 gain -15 < /dev/zero
